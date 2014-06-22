af286879761aae0bcef11f431a9a3b23398ef9f2
Payam Naderi <naderi.payam@gmail.com>
2014-06-22 13:04:25 +0430

    change model interface

--- src/yimaSettings/Model/Settings.php
+++ src/yimaSettings/Model/Settings.php
@@ -2,6 +2,7 @@
 namespace yimaSettings\Model;
 
 use yimaSettings\Service\Settings\SettingEntity;
+use yimaSettings\Service\Settings\SettingHydrator;
 use Zend\Config\Writer\PhpArray as PhpArrayWriter;
 
 /**
@@ -12,11 +13,16 @@ use Zend\Config\Writer\PhpArray as PhpArrayWriter;
 class Settings implements SettingsInterface
 {
     /**
+     * @var SettingHydrator
+     */
+    protected $hydrator;
+
+    /**
      * Save Entity Properties for a section
      * section is sets of related configs that collect together
      *
-     * @param $namespace
-     * @param SettingEntity $entity
+     * @param string        $namespace Namespace
+     * @param SettingEntity $entity    Entity
      *
      * @return boolean
      */
@@ -30,15 +36,34 @@ class Settings implements SettingsInterface
             unlink($file);
         }
 
-        $writer->toFile($file, $entity->getArrayCopy());
+        $writer->toFile(
+            $file,
+            $this->getHydrator()->extract($entity)
+        );
 
         return true;
     }
 
     /**
+     * Load namespace data and hydrate new data in given entity
+     *
+     * @param string        $namespace Namespace
+     * @param SettingEntity $entity    Entity
+     *
+     * @return array Key/Value Data
+     */
+    public function getNamespaceDataIntoEntity($namespace, SettingEntity $entity)
+    {
+        $data = $this->getDataWithNamespace($namespace);
+        $this->getHydrator()->hydrate($data, $entity);
+
+        return $data;
+    }
+
+    /**
      * Load Entity Properties for a section
      *
-     * @param $namespace
+     * @param string $namespace Namespace
      *
      * @return array
      */
@@ -52,5 +77,19 @@ class Settings implements SettingsInterface
         }
 
         return $return;
+    }
+
+    /**
+     * Get Hydrator
+     *
+     * @return SettingHydrator
+     */
+    protected function getHydrator()
+    {
+        if (!$this->hydrator) {
+            $this->hydrator = new SettingHydrator();
+        }
+
+        return $this->hydrator;
     }
 }
--- src/yimaSettings/Model/SettingsInterface.php
+++ src/yimaSettings/Model/SettingsInterface.php
@@ -14,17 +14,27 @@ interface SettingsInterface
      * Save Entity Properties for a section
      * section is sets of related configs that collect together
      *
-     * @param $namespace
-     * @param SettingEntity $entity
+     * @param string        $namespace Namespace
+     * @param SettingEntity $entity    Entity
      *
-     * @return mixed
+     * @return boolean
      */
     public function updateWithNamespace($namespace, SettingEntity $entity);
 
     /**
+     * Load namespace data and hydrate new data in given entity
+     *
+     * @param string        $namespace Namespace
+     * @param SettingEntity $entity    Entity
+     *
+     * @return array Key/Value Data
+     */
+    public function getNamespaceDataIntoEntity($namespace, SettingEntity $entity);
+
+    /**
      * Load Entity Properties for a section
      *
-     * @param $namespace
+     * @param string $namespace Namespace
      *
      * @return array
      */
