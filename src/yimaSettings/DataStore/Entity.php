088dbf9d30316190a6793a9412979a0383e83c9a
Payam Naderi <naderi.payam@gmail.com>
2014-09-24 12:18:20 +0330

    implement entity functionality

--- src/yimaSettings/DataStore/Entity.php
+++ src/yimaSettings/DataStore/Entity.php
@@ -1,6 +1,8 @@
 <?php
 namespace yimaSettings\DataStore;
 
+use yimaSettings\DataStore\Exception\PropertyNotFoundException;
+use yimaSettings\DataStore\Entity\Converter\ArrayConverter;
 use yimaSettings\DataStore\Entity\ConverterInterface;
 
 class Entity
@@ -8,17 +10,55 @@ class Entity
     const DEFAULT_NONE_VALUE = null;
 
     /**
+     * Entity's items
+     *
+     * @var array
+     */
+    protected $properties = array();
+
+    /**
+     * Construct
+     *
+     * @param array $props
+     */
+    final public function __construct($props = array())
+    {
+        if (!empty($this->properties)) {
+            // maybe we have some predefined props field in class
+            // protected properties = array( .... );
+            $props = array_merge($this->properties, $props);
+        }
+
+        $this->setFrom(new ArrayConverter($props));
+
+        $this->conit();
+    }
+
+    /**
+     * Init Entity after construct
+     */
+    protected function conit()
+    {
+
+    }
+
+    /**
      * Get Property
      *
      * @param $prop
      * @param null $default Default Empty None Value
      *
-     * @throw Exception\PropertyNotFoundException
+     * @throw PropertyNotFoundException
      * @return mixed
      */
     public function get($prop, $default = self::DEFAULT_NONE_VALUE)
     {
+        if (!$this->has($prop))
+            throw new PropertyNotFoundException(
+                sprintf('Property "%s" not found in entity.', $prop)
+            );
 
+        return $this->properties[$prop];
     }
 
     /**
@@ -31,7 +71,9 @@ class Entity
      */
     public function set($prop, $value = self::DEFAULT_NONE_VALUE)
     {
+        $this->properties[$prop] = $value;
 
+        return $this;
     }
 
     /**
@@ -43,7 +85,7 @@ class Entity
      */
     public function has($prop)
     {
-
+        return isset($this->properties[$prop]);
     }
 
     /**
@@ -51,12 +93,14 @@ class Entity
      *
      * @param string $prop Property
      *
-     * @throw Exception\PropertyNotFoundException
      * @return $this
      */
     public function del($prop)
     {
+        if (!$this->has($prop))
+            unset($this->properties[$prop]);
 
+        return $this;
     }
 
     /**
@@ -66,7 +110,7 @@ class Entity
      */
     public function keys()
     {
-
+        return array_keys($this->properties);
     }
 
     /**
